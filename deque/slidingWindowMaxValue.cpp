ebd936863f807cfce414cc5679b6f12d4ed764ee
satishagrwal <satish.agrwal@gmail.com>
2021-07-16 20:15:49 +0530

    Update slidingWindowMaxValue.cpp

--- deque/slidingWindowMaxValue.cpp
+++ deque/slidingWindowMaxValue.cpp
@@ -18,6 +18,25 @@ Window position                Max
 
 #include <bits/stdc++.h>
 using namespace std;
+
+/*
+void printKMax(int arr[], int n, int k)
+{
+    int j, max;
+ 
+    for (int i = 0; i <= n - k; i++)
+    {
+        max = arr[i];
+ 
+        for (j = 1; j < k; j++)
+        {
+            if (arr[i + j] > max)
+                max = arr[i + j];
+        }
+        cout << max << " ";
+    }
+}
+*/
  
 void printKMax(int arr[], int n, int k)
 {
